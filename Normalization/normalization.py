9a77335e919c728387f47093fbe2dba4cc4fd5ef
Diptam Paul <diptampaul13@gmail.com>
2020-12-05 11:39:25 +0530

    Normalization removed 2

--- Normalization/normalization.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-
-data = pd.read_csv('upload_fileupload.csv')
-
-#Removing Duplicates and keeping last one
-data = data.drop_duplicates(subset=['file_uploaded','name'], keep='last', inplace=True)
-print(data)
